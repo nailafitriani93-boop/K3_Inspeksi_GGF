d65c431c61cf2bba3d8df68c0c29572e7324ea40
Naila <hidayfitri41@gmail.com>
2026-09-18 17:00:29 +0700

    Set GGF favicon

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,10 @@ import "./globals.css";
 
 export const metadata = {
   title: "Inspeksi K3 - Great Giant Foods",
-  description: "Monitoring inspeksi K3 dan tindak lanjut temuan"
+  description: "Monitoring inspeksi K3 dan tindak lanjut temuan",
+  icons: {
+    icon: "/ggf-estate-pg01.png",
+  },
 };
 
 export default function RootLayout({ children }) {
